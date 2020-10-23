445a8ce515d218483d838021e09f06851e43c535
Kalissaac <kalissaac@protonmail.com>
2020-10-22 21:31:58 -0700

    Add routes and middleware

--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module web
 
 go 1.15
 
-require github.com/gofiber/fiber/v2 v2.1.1
+require (
+	github.com/0xAX/notificator v0.0.0-20191016112426-3962a5ea8da1 // indirect
+	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
+	github.com/codegangsta/gin v0.0.0-20171026143024-cafe2ce98974 // indirect
+	github.com/gofiber/fiber/v2 v2.1.1
+	github.com/klauspost/compress v1.11.1 // indirect
+	github.com/mattn/go-shellwords v1.0.10 // indirect
+	golang.org/x/sys v0.0.0-20201022201747-fb209a7c41cd // indirect
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
+)
--- web.go
+++ web.go
@@ -4,15 +4,37 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
+
+var (
+	app = fiber.New(fiber.Config{
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			return c.Status(500).SendString(err.Error())
+		},
+	})
+)
+
+func setupMiddleware() {
+	app.Use(compress.New())
+	app.Use(limiter.New())
+}
+
+func setupRoutes() {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to hoopback!")
+	})
+
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusNotFound).SendString("Sorry can't find that!")
+	})
+}
 
 func main() {
 	port := os.Getenv("PORT")
-	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("welcome to hoopback")
-	})
-
+	setupMiddleware()
+	setupRoutes()
 	app.Listen(":" + port)
 }
