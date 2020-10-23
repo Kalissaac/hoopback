6582b099967890a2974e5dc66ea715ae3cbc8849
Kalissaac <kalissaac@protonmail.com>
2020-10-23 14:53:02 -0700

    Add MongoDB and fix authentication

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/joho/godotenv v1.3.0
 	github.com/klauspost/compress v1.11.1 // indirect
 	github.com/mattn/go-shellwords v1.0.10 // indirect
+	go.mongodb.org/mongo-driver v1.4.2
 	golang.org/x/sys v0.0.0-20201022201747-fb209a7c41cd // indirect
 	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
 )
--- web.go
+++ web.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -11,8 +12,11 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var (
@@ -21,33 +25,33 @@ var (
 			return c.Status(500).SendString(err.Error())
 		},
 	})
+	client *mongo.Client
 )
 
 func setupMiddleware() {
 	app.Use(compress.New())
-	app.Use(limiter.New())
+	// app.Use(limiter.New())
 }
 
-func setupRoutes() {
+func setupAuth() {
 	app.Get("/login", func(c *fiber.Ctx) error {
-		fmt.Println(c.Params("code"))
 		redirectURI := c.BaseURL() + "/login"
+
 		if c.Cookies("authorization") != "" {
 			// Authorization cookie found, user is authenticated
-			return c.Redirect("/")
-		} else if c.Params("code") != "" {
+			return c.Redirect("/restricted")
+		} else if c.Query("code") != "" {
 			// Authorization code from Discord found
 			data := url.Values{
 				"client_id":     {os.Getenv("CLIENT_ID")},
 				"client_secret": {os.Getenv("CLIENT_SECRET")},
 				"grant_type":    {"authorization_code"},
 				"redirect_uri":  {redirectURI},
-				"code":          {c.Params("code")},
+				"code":          {c.Query("code")},
 				"scope":         {"identify"},
 			}
 
 			resp, err := http.PostForm("https://discordapp.com/api/oauth2/token", data)
-
 			if err != nil {
 				panic(err)
 			}
@@ -68,15 +72,73 @@ func setupRoutes() {
 
 			// Set cookie
 			c.Cookie(cookie)
-			return c.Redirect("/")
+
+			return c.Redirect("/restricted")
 		} else {
 			// If no auth cookie is found, and it's not a redirect from Discord, send them to login
 			return c.Redirect("https://discord.com/api/oauth2/authorize?client_id=768975292621783089&redirect_uri=" + url.QueryEscape(redirectURI) + "&response_type=code&scope=identify")
 		}
 	})
 
+	app.Use(func(c *fiber.Ctx) error {
+		// TODO: Add authorization token verification
+		if c.Cookies("authorization") == "" {
+			return c.Redirect("/login")
+		}
+		return c.Next()
+	})
+}
+
+// User object
+type User struct {
+	ID           string `json:"_id,omitempty"`
+	Title        string `json:"title"`
+	Body         string `json:"body"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+func _createUser(userID *string) {
+	collection := client.Database("data").Collection("users")
+
+	user := User{
+		ID:    *userID,
+		Title: "bob",
+		Body:  "john",
+	}
+
+	insertResult, err := collection.InsertOne(context.TODO(), user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted post with ID:", insertResult.InsertedID)
+}
+
+func initUser(userID *string) {
+	collection := client.Database("data").Collection("users")
+
+	var user User
+
+	err := collection.FindOne(context.TODO(), bson.D{primitive.E{Key: "_id", Value: userID}}).Decode(&user)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			_createUser(userID)
+			return
+		}
+		log.Fatal(err)
+	}
+	return
+}
+
+func setupRoutes() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Welcome to hoopback!")
+	})
+
+	setupAuth()
+
+	app.Get("/restricted", func(c *fiber.Ctx) error {
+		return c.SendString("restricted area!1!!")
 	})
 
 	app.Use(func(c *fiber.Ctx) error {
@@ -89,9 +151,23 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	port := os.Getenv("PORT")
 
 	setupMiddleware()
 	setupRoutes()
+
+	port := os.Getenv("PORT")
 	app.Listen(":" + port)
+
+	client, err = mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	defer cancel()
 }
