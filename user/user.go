9e8a0e6516343d86d0f25a48b1a563aa499c21f4
Kalissaac <kalissaac@protonmail.com>
2020-11-02 08:05:56 -0800

    Add basic home page

--- user/user.go
+++ user/user.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
@@ -24,7 +23,7 @@ func Setup(a *fiber.App, s *session.Session, c *mongo.Client) {
 	sessions = s
 	client = c
 
-	app.Get("/@me", func(c *fiber.Ctx) error {
+	app.Get("/home", func(c *fiber.Ctx) error {
 		store := sessions.Get(c)
 		var user auth.User
 		err := client.Database("data").Collection("users").FindOne(context.TODO(), bson.D{{Key: "_id", Value: store.Get("user")}}).Decode(&user)
@@ -34,6 +33,35 @@ func Setup(a *fiber.App, s *session.Session, c *mongo.Client) {
 			}
 			log.Fatal(err)
 		}
-		return c.SendString(fmt.Sprintf("Welcome to hoopback, %s!", user.Username))
+
+		return c.Render("home", fiber.Map{
+			"user": user,
+		}) // , "layouts/main"
+	})
+
+	app.Get("/w/:user/:webhook/edit", func(c *fiber.Ctx) error {
+		var user auth.User
+		err := client.Database("data").Collection("users").FindOne(context.TODO(), bson.D{{Key: "_id", Value: c.Params("user")}}).Decode(&user)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				return fiber.NewError(fiber.StatusNotFound, "User not found! Are they registered?")
+			}
+			log.Fatal(err)
+		}
+
+		webhook, ok := user.Webhooks[c.Params("webhook")]
+		if ok == false {
+			return fiber.NewError(fiber.StatusNotFound, "Webhook not found!")
+		}
+
+		store := sessions.Get(c)
+		if store.Get("user") != user.ID {
+			return fiber.NewError(fiber.StatusNotFound, "Webhook not found or user is unauthorized to access this page!")
+		}
+
+		return c.Render("edithook", fiber.Map{
+			"user":    user,
+			"webhook": webhook,
+		}) // , "layouts/main"
 	})
 }
